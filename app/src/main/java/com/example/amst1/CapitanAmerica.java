e664a86eb57bde3b23594182607df8aeb1d0ef90
sofia <sespana@espol.edu.ec>
2020-01-01 22:28:36 -0500

    Culminacion de la aplicacion con los eventos de las activitys

--- app/src/main/java/com/example/amst1/CapitanAmerica.java
+++ app/src/main/java/com/example/amst1/CapitanAmerica.java
@@ -1,7 +1,10 @@
 package com.example.amst1;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class CapitanAmerica extends AppCompatActivity {
@@ -10,5 +13,9 @@ public class CapitanAmerica extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capitan_america);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.user_name);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#2D59CE")));
     }
 }
--- app/src/main/java/com/example/amst1/Fury.java
+++ app/src/main/java/com/example/amst1/Fury.java
@@ -1,7 +1,10 @@
 package com.example.amst1;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class Fury extends AppCompatActivity {
@@ -10,5 +13,9 @@ public class Fury extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fury);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.user_name);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#747474")));
     }
 }
